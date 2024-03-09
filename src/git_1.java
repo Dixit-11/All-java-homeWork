07bbf04bf5f746a893fed3d7cbb475ab99a1d660
Dixit <dixit.testing11@gmail.com>
2024-03-09 14:25:54 +0000

    Git1

---
+++ src/git_1.java
@@ -0,0 +1,15 @@
+public class git_1 {
+
+   static int x = 10;
+    static int z = 10;
+
+    public static void main(String[] args){
+
+        System.out.println(x);
+        System.out.println(z);
+
+
+    }
+
+
+}
