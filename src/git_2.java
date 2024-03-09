a3ce397d622b89a8e5c88fe9ff2984c46aeaef1d
Dixit <dixit.testing11@gmail.com>
2024-03-09 15:17:39 +0000

    add changes

---
+++ src/git_2.java
@@ -0,0 +1,14 @@
+public class git_2 {
+    static int x = 10;
+    static int z = 10;
+
+    static int c = 20;
+
+    public static void main(String[] args) {
+
+        System.out.println(x);
+        System.out.println(z);
+
+
+    }
+}
