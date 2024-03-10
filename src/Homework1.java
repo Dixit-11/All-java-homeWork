2469936c54b5db5c008239bbf8b59bcfefd4ddb7
Dixit <dixit.testing11@gmail.com>
2024-03-10 20:56:05 +0000

    in to new repository

--- src/Homework1.java
+++ src/Homework1.java
@@ -11,7 +11,7 @@ public class Homework1 {
     public static void main(String[] args){
         System.out.println(firstname + Lastname); // using parameter
         System.out.println(" Mobile number 07492222757 "); // using non parameter
-
+        System.out.println(" Age " + age );
         System.out.println ( " salary £ " + salary );
         System.out.println(" dixit " + " kachhia " + " is " +  age + " year old" );// string concatenating
     }
